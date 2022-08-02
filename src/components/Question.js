285148957aa17ecd126326a5b1e34fcd33a56fc4
Shuber <shuberinu@gmail.com>
2022-08-02 11:01:48 +0700

    conditional styling for answers using class names

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,6 +1,22 @@
 import './Question.css';
 
-const Question = () => {
+	const className = (answer) => {
+		let name = 'answer';
+		let isCorrect = answer.answer === props.correctAnswer;
+
+		if (answer.isSelected) {
+			name = name + ' answer-selected';
+		}
+		if (props.finished && isCorrect) {
+			name = name + ' answer-correct';
+		}
+
+		if (props.finished && answer.isSelected && !isCorrect) {
+			name = name + ' answer-incorrect';
+		}
+
+		return name;
+	};
 	return (
 		<div className='question-group'>
 			<h2>How would one say goodbye in Spanish?</h2>
