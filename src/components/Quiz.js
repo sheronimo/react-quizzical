a95c0b373788d31b4d7eb3879d93aeb7a8179dd8
Shuber <shuberinu@gmail.com>
2022-08-02 10:58:25 +0700

    function for fetching trivia questions

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -8,6 +8,28 @@ const Quiz = () => {
 	const [loading, setLoading] = useState(false);
 	const [gameFinished, setGameFinished] = useState(false);
 	const [score, setScore] = useState(0);
+	function fetchQuestions() {
+		setLoading(true);
+		fetch(
+			'https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple'
+		)
+			.then((res) => res.json())
+			.then((data) =>
+				setQuestions(
+					data.results.map((r) => ({
+						id: nanoid(),
+						question: r.question,
+						correctAnswer: r.correct_answer,
+						answers: returnAnswers(
+							shuffleArray([r.correct_answer, ...r.incorrect_answers])
+						)
+					}))
+				)
+			)
+			.finally(() => {
+				setLoading(false);
+			});
+	}
 };
 
 export default Quiz;
