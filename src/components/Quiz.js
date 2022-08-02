85be717ea25e41fe856419c68d4ac26e5370adcc
Shuber <shuberinu@gmail.com>
2022-08-02 10:59:48 +0700

    fisher-yates shuffle algo for answers

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -43,6 +43,18 @@ const Quiz = () => {
 			isSelected: false
 		}));
 	}
+
+	function shuffleArray(array) {
+		let i = array.length - 1;
+		for (; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			const temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+		return array;
+	}
+
 };
 
 export default Quiz;
